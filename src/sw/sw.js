29303eea7a6f28d8d7eec81594e7b84fe0ef9417
Ilya Kreymer <ikreymer@gmail.com>
2022-11-23 22:01:55 -0800

    switch to @webrecorder/awp-sw import to ipfs using awp-sw API add options to ipfs serialization in awp-sw, including custom chunking cleanup, remove separate web3 uploader impl

--- src/sw/sw.js
+++ src/sw/sw.js
@@ -1,15 +1,14 @@
 
 import { SWReplay } from "@webrecorder/wabac/src/swmain.js";
 
-import { ExtAPI } from "awp-core/src/index.js";
-import { RecordingCollections } from "awp-core/src/index.js";
+import { ExtAPI, RecordingCollections } from "@webrecorder/awp-sw";
 
 // ===========================================================================
 export class UpdatingAPI extends ExtAPI
 {
   constructor(collections) {
     // eslint-disable-next-line no-undef
-    super(collections, {softwareString: `Save Tweet Now ${__AWP_EXPRESS_VERSION__}, using `});
+    super(collections, {softwareString: `Save Tweet Now ${__VERSION__}, using `});
   }
 }
 
